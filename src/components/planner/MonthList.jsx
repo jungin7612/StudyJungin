eedd375a964f9eab28d123c71ea37827ace3a6d3
jungin7612 <jungin7612@gmail.com>
2021-07-31 00:02:56 +0900

    feat : CSR 무한 loop 해결

--- src/components/planner/MonthList.jsx
+++ src/components/planner/MonthList.jsx
@@ -1,6 +1,29 @@
+import { useEffect } from "react";
 import classes from "./monthList.module.css";
 
 export default function MonthList({ posts, setPosts }) {
+  useEffect(() => {
+    const data = {
+      id: "2021-07",
+    };
+    fetch("https://studyapi.jungin.me/api/getdata", {
+      method: "POST",
+      body: JSON.stringify(data), //
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        datatype: "text",
+      },
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        if (response) {
+          setPosts(response);
+        } else {
+          alert("something wrong");
+        }
+      });
+  }, [])
+
   const getTodos = (e) => {
     const data = {
       id: e.target.id,
